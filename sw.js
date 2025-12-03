77605f89e9922f6cc0d673f822615cb718cc5ab0
Tomáš Lapos <65809639+tomaslapos@users.noreply.github.com>
2025-12-03 18:17:55 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Umožňuje offline funkčnost a rychlejší načítání
  */
 
-const CACHE_NAME = 'gtg-zavady-v2';
+const CACHE_NAME = 'gtg-zavady-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -20,7 +20,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Otevírám cache v2');
+                console.log('Otevírám cache v3');
                 return cache.addAll(ASSETS_TO_CACHE);
             })
             .then(() => {
